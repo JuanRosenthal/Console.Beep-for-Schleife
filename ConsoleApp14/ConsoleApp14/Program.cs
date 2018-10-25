c2fa2be390f2e6700c748aa192077de275dda290
Juan Rosenthal <Juan-rosenthal@outlook.com>
2018-10-26 00:16:46 +0200

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Start:
+
+            Console.WriteLine("wie oft soll es piepsen?");
+
+            
+                double b = Convert.ToDouble(Console.ReadLine());
+        
+           
+
+            for (double a = 1; a <= b; a = a + 1)
+            {
+                double d = b - a;                
+                Console.WriteLine("beeps verbleibend: "+ d);
+                Console.Beep();
+                Console.Clear();
+            }
+            Console.Clear();            
+            goto Start;
+
+        }
+    }
+}
